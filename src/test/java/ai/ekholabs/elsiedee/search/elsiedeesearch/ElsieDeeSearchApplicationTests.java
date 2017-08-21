bbac70ac865d87d3bfb3d0e1ec9eac23989b8bc1
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-08-21 12:11:29 +0200

    Add gradle neture to the project + Spring Boot/Cloud dependencies..

---
+++ src/test/java/ai/ekholabs/elsiedee/search/elsiedeesearch/ElsieDeeSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package ai.ekholabs.elsiedee.search.elsiedeesearch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElsieDeeSearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
