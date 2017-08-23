026e4d11f168df3891e6dfaffc12a9cb61f87a9a
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-08-23 11:15:54 +0200

    Use multipart file for subtitles and request param for title.

--- src/main/java/ai/ekholabs/elsiedee/search/controller/ElsieDeeAssetController.java
+++ src/main/java/ai/ekholabs/elsiedee/search/controller/ElsieDeeAssetController.java
@@ -1,6 +1,10 @@
 package ai.ekholabs.elsiedee.search.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ai.ekholabs.elsiedee.search.model.Acknowledge;
 import ai.ekholabs.elsiedee.search.model.Asset;
@@ -10,10 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import static java.util.Arrays.asList;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
 public class ElsieDeeAssetController {
@@ -30,8 +37,17 @@ public class ElsieDeeAssetController {
     return elasticSearchService.createIndex(Asset.class);
   }
 
-  @PostMapping(value = "/createAsset", consumes = APPLICATION_JSON_UTF8_VALUE, produces = APPLICATION_JSON_UTF8_VALUE)
-  public Asset createAsset(@RequestBody final Asset asset) {
+  @PostMapping(value = "/createAsset", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_UTF8_VALUE)
+  public Asset createAsset(@RequestParam(value = "title") final String title,  final @RequestParam(value = "subtitles") MultipartFile subtitles)
+      throws IOException {
+
+    final StringBuilder builder = new StringBuilder();
+    try (BufferedReader buffer = new BufferedReader(new InputStreamReader(subtitles.getInputStream()))) {
+      final String subtitlesText = buffer.lines().collect(Collectors.joining("\n"));
+      builder.append(subtitlesText);
+    }
+
+    final Asset asset = new Asset(title, builder.toString());
     return elasticSearchService.createAsset(asset);
   }
 
