2e2ee5d883ebc776dcd282d6c094106815704547
Wilder Rodrigues <wilder.rodrigues@irdeto.com>
2017-08-24 22:43:10 +0200

    Receive subtitles in the request body.

--- src/main/java/ai/ekholabs/elsiedee/search/controller/ElsieDeeAssetController.java
+++ src/main/java/ai/ekholabs/elsiedee/search/controller/ElsieDeeAssetController.java
@@ -1,29 +1,23 @@
 package ai.ekholabs.elsiedee.search.controller;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import ai.ekholabs.elsiedee.search.model.Acknowledge;
 import ai.ekholabs.elsiedee.search.model.Asset;
 import ai.ekholabs.elsiedee.search.model.AssetKeyword;
 import ai.ekholabs.elsiedee.search.model.Subtitles;
 import ai.ekholabs.elsiedee.search.service.ElasticSearchService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
-import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
 public class ElsieDeeAssetController {
@@ -42,18 +36,10 @@ public class ElsieDeeAssetController {
     return elasticSearchService.createIndex(Asset.class);
   }
 
-  @PostMapping(value = "/createAsset", consumes = MULTIPART_FORM_DATA_VALUE, produces = APPLICATION_JSON_UTF8_VALUE)
-  public Asset createAsset(final @RequestParam(value = "title") String title,
-                           final @RequestParam(value = "subtitles") MultipartFile subtitlesFile) throws IOException {
-
-    final StringBuilder builder = new StringBuilder();
-    try (BufferedReader buffer = new BufferedReader(new InputStreamReader(subtitlesFile.getInputStream()))) {
-      final String subtitlesText = buffer.lines().collect(Collectors.joining(" "));
-      builder.append(subtitlesText);
-    }
-
-    final Subtitles subtitles = new ObjectMapper().readValue(builder.toString(), Subtitles.class);
-    final Asset asset = new Asset(title, subtitles);
+  @PostMapping(value = "/createAsset/{title}", consumes = APPLICATION_JSON_UTF8_VALUE, produces = APPLICATION_JSON_UTF8_VALUE)
+  public Asset createAsset(final @PathVariable(value = "title") String assetTitle,
+                           final @RequestBody Subtitles subtitles) throws IOException {
+    final Asset asset = new Asset(assetTitle, subtitles);
 
     return elasticSearchService.createAsset(asset);
   }
